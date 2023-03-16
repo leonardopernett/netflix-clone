2b97a682019659ce12e62a274a22a9d4ea111f06
Leonardo Pernett Jimenez <lpernett@gmail.com>
2023-03-16 15:25:07 -0600

    add login

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,15 +1,23 @@
 import Input from "@/components/Input";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export default function auth() {
 
 
   const [input, setInput] = useState({
+    username:'',
     email:'',
     password:''
   })
+  const [variant, setvariant] = useState("login")
 
   const inputChage = (e:any) => setInput({...input, [e.target.name]:e.target.value})
+
+  const toggleVariant = useCallback(() => {
+    console.log('renderizado')
+      setvariant(prev => prev === 'login' ? 'register':'login')
+  },[])
+   
 
   return (
     <div className="relative w-full h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
@@ -22,9 +30,21 @@ export default function auth() {
          <div className="flex justify-center items-center">
             <div className="bg-black rounded-md bg-opacity-70 px-16 py-16 self-center mt-2 lg:max-w-md  lg:w-2/5">
                 <h2 className="text-white font-semibold text-2xl mb-5">
-                   Signin
+                   {variant ==='login' ? 'Sign in' : 'Register' }
                 </h2>
                 <div className="flex flex-col w-full">
+                   {
+                      variant ==='register' && (
+                        <Input 
+                          type="text" 
+                          value={ input.email } 
+                          name="username"
+                          id="username"
+                          onChange={ inputChage }
+                          placeholder="Username"
+                        />
+                      )
+                   }
                   <Input 
                     type="email" 
                     value={ input.email } 
@@ -42,7 +62,15 @@ export default function auth() {
                     onChange={ inputChage }
                     placeholder="*******"
                   />
-                  <button className="bg-red-600 hover:bg-red-700 rounded-md text-white font-semibold py-2">Login</button>
+                  <button className="bg-red-600 hover:bg-red-700 rounded-md text-white font-semibold py-2">
+                     { variant ==='login' ? 'Login' : 'Sign up' }
+                  </button>
+                  <p className="text-neutral-500 mt-10">   { variant ==='login' ? 'First time using Netflix? ' : 'Already have an account? ' } 
+                     <span className="text-white hover:underline cursor-pointer" onClick={toggleVariant}>
+                     { variant ==='login' ? 'Create and account' : 'Login' } 
+                       </span>
+                  </p>
+              
                 </div>
             </div>
          </div>
